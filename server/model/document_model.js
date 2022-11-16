9546cc27e3212395e7b43f56f469e67003867453
Tanvir <ibnmizan7878@gmail.com>
2022-11-17 00:35:11 +0600

    added doc schema

---
+++ server/model/document_model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const docSchema = mongoose.Schema({
+
+ uid: {
+  type: String,
+  required: true,
+ },
+ createdAt: {
+  type: Number,
+  required: true,
+ },
+ createdAt: {
+  type: String,
+  required: true,
+  trim: true
+ },
+ content: {
+  type: Array,
+  default: [],
+ }
+
+});
+
+const Document = mongoose.model("Document", docSchema)
+
+
+module.exports = Document;
